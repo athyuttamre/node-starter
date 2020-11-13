12e8fd9d328cbb28316743756ce93ae79296c516
Athyuttam Eleti <athyuttamre@gmail.com>
2020-11-12 20:24:39 -0800

    Combine deploys, add top level scripts

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,13 @@
+import url from "url";
+import path from "path";
 import express from "express";
 import db from "./database.js";
 import "./models/sync.js";
 
 // Environment
 const { PORT } = process.env;
+const currentDir = path.dirname(url.fileURLToPath(import.meta.url));
+const staticDir = path.join(currentDir, "../..", "client", "build");
 
 // Database
 try {
@@ -16,8 +20,14 @@ try {
 // Express
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.get("/api/ping", (_, res) => {
+  res.send({ ping: "pong" });
+});
+
+app.use(express.static(staticDir));
+
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(staticDir, "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
